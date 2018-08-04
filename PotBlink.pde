5bb22f6c5f8d50db52b55630789c29287c19265a
Dr. Michael Vogt <doc_vogt@hotmail.com>
2018-08-04 15:27:46 -0400

    snippets
    snippets for the Arduino Uno

---
+++ PotBlink.pde
@@ -0,0 +1,32 @@
+/*
+  PotBlink modifications MCVogt 31Jan2012
+  Turns on LED on Port 13 to heartbeat w Pot-controlled delay between blinkblinks...
+  
+  PotBlink
+  This example code is in the public domain.
+ */
+//declare these variables outside of setup so scope is global...
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin 
+
+void setup() {                
+  // initialize the digital pin as an output.
+  // Pin 13 has an LED connected on most Arduino boards:
+  pinMode(13, OUTPUT);     
+}
+
+void loop() {
+  digitalWrite(13, HIGH);   // set the LED on
+  delay(100);              // wait for a second
+  digitalWrite(13, LOW);    // set the LED off
+  delay(100);              // wait for a second
+  digitalWrite(13, HIGH);   // set the LED on
+  delay(100);              // wait for a second
+  digitalWrite(13, LOW);    // set the LED off
+
+  //control the value of the following delay using the POT input value...
+  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023) 
+  val = map(val, 0, 1023, 0, 2000);     // scale it to use it as a 0->2 Second (value between 0 and 2000 ms) 
+
+  delay(val);              // wait for two seconds
+}
---
+++ ThreeBlink.ino
@@ -0,0 +1,40 @@
+/*
+Apr2012    MCVogt
+This code uses a Toshiba Tecra M7 running Windows 7 using Serial Port COM4 to communicate with
+an Arduino Uno...
+*/
+
+int sensorPin = A0;   // select the input pin for the NH3 sensor...
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {                
+  Serial.begin(115200);
+
+  // initialize the digital pins as an output.
+  pinMode(5, OUTPUT); //RED LED... 
+  pinMode(6, OUTPUT); //BLUE LED...    
+  pinMode(7, OUTPUT); //GREEN LED...
+}
+
+void loop() {
+  digitalWrite(7, HIGH);   // set the RED LED on
+  delay(100);              // wait for a second
+  digitalWrite(7, LOW);    // set the LED off
+  delay(100);              // wait for a second
+
+  digitalWrite(6, HIGH);   // set the GREEN LED on
+  delay(100);              // wait for a second
+  digitalWrite(6, LOW);    // set the LED off
+  delay(100);              // wait for a second
+
+  digitalWrite(5, HIGH);   // set the BLUE LED on
+  delay(100);              // wait for a second
+  digitalWrite(5, LOW);    // set the LED off
+  delay(100);              // wait for a second
+
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);    
+
+  Serial.println("current raw NH3 reading is: " + String(sensorValue));
+  delay(1000);          
+}
